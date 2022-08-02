bbef50f837411a2e766db16913a93daa29e57a8a
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-08-02 23:39:55 +0500

    'AAJ'

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log('Text Cliked')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View style={styles.container}>
+    //   <Text>Hello World</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
+
+
+    <SafeAreaView style={styles.container}>
+
+      <Text 
+      numberOfLines={1}
+      onPress={handlePress}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+      </Text>
+
+      {/* <Image style={{width : 300, height: 300}}
+      source={require('./assets/icon.png')}
+      /> */}
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+
   );
+
 }
+console.log("app executed");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
